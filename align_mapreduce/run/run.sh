5481e65655b559b1e4ac51ff01d5a2f0fe767cac
Your Name <you@example.com>
2018-05-25 16:54:25 +0000

    scipts

--- align_mapreduce/run/run.sh
+++ align_mapreduce/run/run.sh
@@ -22,11 +22,11 @@
 
 
 #hdfs dfs -rm -r /home/hadoop/mapred/output  /user/hadoop/mapred 
-hdfs dfs -rm -r /user/hadoop/temp
-hdfs dfs -rm -r bypass_mr
-./clear_emit.sh
+#hdfs dfs -rm -r /user/hadoop/temp
+#hdfs dfs -rm -r bypass_mr
+#./clear_emit.sh
 #time yarn jar align.jar WithoutBowtie -ifile biodata/humandata_100m.fa -ofile bypass_mr/output -rfile /home/hadoop/bio/chr_ref_genomes/splitted -bypass True -machines 20
-time yarn jar align.jar Bowtie -ifile fa_data/100.fa -ofile bowtie_big/output -rfile /home/hadoop/bio/chr_ref_genomes/splitted -machines 20
+time yarn jar align.jar Align -ifile fa_data/100.fa -ofile output -rfile fa_data/chr1.fa -machines 20
 
 #hdfs dfs -rm -r from/output
 #time yarn jar align.jar From -ifile bowtie/output  -ofile from/output -rfile /home/hadoop/bio/chr_ref_genomes/splitted -machines 20
--- align_mapreduce/run/setFile.sh
+++ align_mapreduce/run/setFile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-hdfs dfs -mkdir data_genomes
-hdfs dfs -put /home/hadoop/bio/data_genomes/sample.filtered.fa data_genomes/sample.filtered.fa
+#hdfs dfs -mkdir data_genomes
+#hdfs dfs -put /home/hadoop/bio/data_genomes/sample.filtered.fa data_genomes/sample.filtered.fa
+
+hdfs dfs -put ~/100.fa fa_data/
+hdfs dfs -put ~/chr1.fa fa_data/
