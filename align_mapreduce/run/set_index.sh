403850c147453728f27d6da7737020b1cf7a020e
Your Name <you@example.com>
2018-06-17 16:19:54 +0000

    add slaves to known hosts

--- align_mapreduce/run/set_index.sh
+++ align_mapreduce/run/set_index.sh
@@ -6,5 +6,6 @@ local_path=/home/hadoop/bio/chr_ref_genomes/
 for X in $list
 do
 	echo ssh ubuntu@$X hdfs dfs -get $ref_path $local_path
+	ssh-keyscan -H $X 2>/dev/null 1>> ~/.ssh/known_hosts
 	ssh ubuntu@$X /home/ubuntu/hadoop/bin/hdfs dfs -get $ref_path/* $local_path
 done
